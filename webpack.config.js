ef991c3e4f01d59b9c437ae1054b894cc33173d5
mint <danielavargashidalgo@gmail.com>
2022-04-04 01:07:48 -0500

    commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+   entry: './src/app/index.js',
+   output: {
+     path:  __dirname + '/src/public',
+     filename: 'bundle.js'
+    
+  },
+      module: {
+       rules: [
+       {
+         use: 'babel-loader',
+         test: /\.js$/,
+         exclude: /node_modules/
+       }
+     ]
+   }
+ }; 
+
+//bundle => es todo el codigo convertido y compactado
